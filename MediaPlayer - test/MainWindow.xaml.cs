14b46b478ea5bcdcc92d5a191ac4362ffc6d92fb
rocejfi15 <rocejfi15@DST20717>
2018-01-11 11:50:26 +0100

    xd

--- MediaPlayer - test/MainWindow.xaml.cs
+++ MediaPlayer - test/MainWindow.xaml.cs
@@ -20,9 +20,87 @@ namespace MediaPlayer___test
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        public MainWindow() 
+{ 
+    InitializeComponent(); 
+    IsPlaying(false); 
+} 
+ 
+private void IsPlaying(bool flag) 
+{ 
+    btnPlay.IsEnabled = flag; 
+    btnMoveBack.IsEnabled = flag; 
+    btnMoveForward.IsEnabled = flag; 
+} 
+ 
+private void btnPlay_Click(object sender, RoutedEventArgs e) 
+{ 
+    IsPlaying(true); 
+    if (btnPlay.Content.ToString() == "Play") 
+    { 
+        MediaPlayer.Play(); 
+        btnPlay.Content = "Pause"; 
+    } 
+    else 
+    { 
+        MediaPlayer.Pause(); 
+        btnPlay.Content = "Play"; 
+    } 
+}
+
+ private void btnMute_Click(object sender, RoutedEventArgs e)
+    {
+        IsPlaying(true);
+            if (btnPlay.Content.ToString() == "Mute")
+            {
+                MediaPlayer.IsMuted = false;
+                btnPlay.Content = "Unmute";
+            }
+            else
+            {
+                MediaPlayer.IsMuted = true;
+                btnPlay.Content = "Mute";
+            }
+            
+
+
+    }
+
+private void ChangeMediaVolume(object sender, RoutedPropertyChangedEventArgs<double> args)
+{
+            MediaPlayer.Volume = (double)volumeSlider.Value;
+}
+
+
+
+        private void btnMoveBack_Click(object sender, RoutedEventArgs e) 
+{ 
+    MediaPlayer.Position -= TimeSpan.FromSeconds(10); 
+} 
+ 
+private void btnMoveForward_Click(object sender, RoutedEventArgs e) 
+{ 
+    MediaPlayer.Position += TimeSpan.FromSeconds(10); 
+} 
+ 
+private void btnOpen_Click(object sender, RoutedEventArgs e) 
+{ 
+    // Configure open file dialog box 
+    Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog(); 
+    dialog.FileName = "Videos"; // Default file name 
+    dialog.DefaultExt = ".WMV"; // Default file extension 
+    dialog.Filter = "WMV file (.wm)|*.wmv"; // Filter files by extension  
+ 
+    // Show open file dialog box 
+    Nullable<bool> result = dialog.ShowDialog(); 
+ 
+    // Process open file dialog box results  
+    if (result == true) 
+    { 
+        // Open document  
+        MediaPlayer.Source = new Uri(dialog.FileName); 
+        btnPlay.IsEnabled = true; 
+    } 
+}
     }
 }
